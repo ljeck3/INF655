ed995eafd0bd258f18b947f303d7b1f3e102bd87
lj-1003 <levijustin24@gmail.com>
2026-02-09 15:17:42 -0600

    Task 1 edits

--- assignment1/src/components/Greeting.jsx
+++ assignment1/src/components/Greeting.jsx
@@ -1,11 +1,13 @@
-//Task1
 function Greeting() {
+  const date = new Date();
+
   return (
     <div>
       <h1>Hello, Welcome to React!</h1>
       <p style={{ color: "blue" }}>React is really cool!</p>
+      <p>Today's date: {date.toDateString()}</p>
     </div>
-)
+  );
 }
 
 export default Greeting;
