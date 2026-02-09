071f7236625b1dcdac0282b4192ff93c9cd15324
lj-1003 <levijustin24@gmail.com>
2026-02-09 12:37:55 -0600

    Task 1

---
+++ assignment1/src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+//Task1
+function Greeting() {
+  return (
+    <div>
+      <h1>Hello, Welcome to React!</h1>
+      <p style={{ color: "blue" }}>React is really cool!</p>
+    </div>
+)
+}
+
+export default Greeting;
