dd81f9fe25dfefacf4691a6029e1bde12078d573
lj-1003 <levijustin24@gmail.com>
2026-02-09 22:01:20 -0600

    Task 3 and 4

--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -1,13 +1,30 @@
-import Task1 from './components/Greeting'
+import Greeting from './components/Greeting'
 import UserInfo from './components/UserInfo'
 
 function App() {
+  //task3
+  const tasks = [
+    "Take out the trash",
+    "Do the dishes",
+    "Clean the bathroom",
+    "Fold laundry",
+    "Wash the floor",
+  ];
+  function getRandomTask() {
+    const randomTask = Math.floor(Math.random() * tasks.length);
+    console.log(randomTask);
+    const result = tasks[randomTask];
+    alert(result);
+  }
   return (
     <div className="App">
-      <Task1 />
+      <Greeting />
       <UserInfo />
-      
+      <button onClick={getRandomTask}>
+        Get Random Task
+      </button>
     </div>
+    
   );
 }
 
